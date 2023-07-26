5cc30aa6fa8d61a25e3f542073b880ebd7d8e3fb
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-07-27 01:33:30 +0530

    Update Main.java

--- Algo/1. Sorting algo/6. Counting Sort/Main.java
+++ Algo/1. Sorting algo/6. Counting Sort/Main.java
@@ -1,21 +1,24 @@
 // We can apply counting sort when the elements are in a particular range
 // t.c.=O(n+range)=O(n) if range=O(n)
-// s.c.=O(range)
+// s.c.=O(range+n)
 import java.util.*;
 
 class Main {
   public static void countingSort(int[] arr, int range) {
-    int[] freq = new int[range+1];
     int n=arr.length;
+    int[] freq = new int[range+1];
+    int[] sortedArr = new int[n];
     for(int i=0;i<n;i++)
       freq[arr[i]]++;
     int j=0;
-    for(int i=0;i<=range;i++) {
-      int cnt=freq[i];
-      while(cnt>0) {
-        arr[j++]=i;
-        cnt--;
-      }
+    for(int i=1;i<=range;i++) {
+      freq[i]+=freq[i-1];
     }
+    for(int i=n-1;i>=0;i--) {
+      sortedArr[freq[arr[i]-1]]=arr[i];
+      freq[arr[i]]--;
+    }
+    for(int i=0;i<n;i++)
+      arr[i]=sortedArr[i];
   }
 }
