874bad4501dd818c1b9c10848382c6932d4bf82e
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-07-26 22:11:06 +0530

    Create Main.java

---
+++ Algo/1. Sorting algo/1. Insertion sort/Main.java
@@ -0,0 +1,19 @@
+// t.c.=O(n) for best case, O(n^2) for worst case and avg case
+// s.c.=O(1), its inplace sorting algorithm
+import java.util.*;
+
+class Main {
+  public static void insertionSort(int[] arr) {
+    int n=arr.length;
+    for(int i=1;i<n;i++) {
+      int key=arr[i];
+      int j=i-1;
+      // place the key(arr[i]) at the correct place in its left side
+      while(j>=0 && key>arr[j]) {
+        arr[j+1]=arr[j];
+        j--;
+      }
+      arr[j+1]=key;
+    }
+  }
+}
