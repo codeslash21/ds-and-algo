f84cee365e3747cd286b6ceb35e96f9106484b70
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-07-27 02:57:38 +0530

    Update DLL.java

--- DS/2. LinkedList/DLL.java
+++ DS/2. LinkedList/DLL.java
@@ -12,17 +12,18 @@ class Node {
   }
 }
 
-class DLL {
-  Node head;
-  int len;
-  public DLL() {
-    head = new Node(0);
-    len=0;
-  }
-
+public class DLL {
   // insert a node before a particular value
-  public void insertBefore(int target, int val) {
-    Node curr=head.next, node = new Node(val);
+  public static Node insertBefore(Node head, int target, int val) {
+    Node curr=head, node = new Node(val);
+    if(head=null)
+      return;
+    if(head.val==target) {
+      node.next=head;
+      head.prev=node;
+      head=node;
+      return head;
+    }
     while(curr!=null) {
       if(curr.val==target)
         break;
@@ -34,5 +35,15 @@ class DLL {
     node.prev=curr.prev;
     curr.prev.next=node;
     curr.prev=node;
+    return head;
   }
+
+  // delete a particular node by value
+  public static void deleteNode(Node head, int target) {
+    if(head==null)
+      return;
+    Node dummy = new Node(0, head), curr=head;
+    while(curr!=null) {
+      if(curr.val==target) {
+        curr.next
 }
